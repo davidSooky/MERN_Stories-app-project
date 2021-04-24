67df421eebe877913323875191f221aa1ba9dc46
David <harold20000@gmail.com>
2021-04-24 14:35:12 +0200

    Backend changes

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,7 +1,10 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 import User from "../models/user.js";
+
+dotenv.config()
 
 export const signin = async (req, res) => {
     const { email, password } = req.body;
@@ -14,7 +17,7 @@ export const signin = async (req, res) => {
 
         if(!isPasswordCorrect) res.status(400).json({message: "Invalid credentials."});
 
-        const token = jwt.sign({email: existingUser.email, id: existingUser._id}, "test", {expiresIn: "1h"});
+        const token = jwt.sign({email: existingUser.email, id: existingUser._id}, process.env.TOKEN_SECRET, {expiresIn: "1h"});
 
         res.status(200).json({result: existingUser, token});
     } catch (error) {
@@ -36,7 +39,7 @@ export const signup = async (req, res) => {
 
         const result = await User.create({email, password: hashedPassword, name: `${firstName} ${lastName}`});
 
-        const token = jwt.sign({email: result.email, id: result._id}, "test", {expiresIn: "1h"});
+        const token = jwt.sign({email: result.email, id: result._id}, process.env.TOKEN_SECRET, {expiresIn: "1h"});
 
         res.status(200).json({result, token});
     } catch (error) {
